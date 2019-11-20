e09d33933342dfe3cf32a6c8820430ad63605607
Andre Ferreira <mrawesomeferreira@gmail.com>
2019-11-20 18:08:00 +0000

    Updates

---
+++ precache-manifest.85956db443469ca2be4611e34bfda887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a63ed7186aebe6aab2fce88cfc9bcab",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "65878f7f7f97efe97737",
+    "url": "/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "82b8d5c47b68911b85bb",
+    "url": "/robofriends/static/css/main.836e6c9b.chunk.css"
+  },
+  {
+    "revision": "65878f7f7f97efe97737",
+    "url": "/robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "82b8d5c47b68911b85bb",
+    "url": "/robofriends/static/js/main.c00f68e5.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
